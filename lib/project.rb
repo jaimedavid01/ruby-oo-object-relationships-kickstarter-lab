c3463bd0b1bd4c60051821f0a25987d2fae95118
jaimedavid01 <jaimedavid01@yahoo.com>
2020-11-21 13:20:55 -0500

    Kickstarter solved

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+    attr_reader :title
+    def initialize(title)
+        @title = title
+    end
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+    def backers_helper
+        #binding.pry
+        ProjectBacker.all.select {|pb| pb.project == self }
+    end
+    def backers
+        self.backers_helper.map { |pb| pb.backer }
+    end
+end
